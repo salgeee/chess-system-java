dabb6fe2d606b033c91be8e97333faf079c57144
Leonardo Salge <lferreirasalge@gmail.com>
2023-01-10 18:54:30 -0300

    Colocando peças no tabuleiro

---
+++ src/Chess/pieces/King.java
@@ -0,0 +1,16 @@
+package Chess.pieces;
+
+import Chess.ChessPiece;
+import Chess.Color;
+import boardgame.Board;
+
+public class King extends ChessPiece {
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
+}
