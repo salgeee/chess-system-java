b5710666c7b56b79aff19861d396c3353ccbfe6f
Leonardo Salge <lferreirasalge@gmail.com>
2023-01-10 21:11:47 -0300

    criando lista de captura

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -7,12 +7,17 @@ import boardgame.Piece;
 import boardgame.Position;
 import Chess.ChessException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMatch {
 
     private int turn;
     private Color currentPlayer;
     private Board board;
 
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
     public ChessMatch() {
         board = new Board(8, 8);
         turn = 1;
@@ -56,6 +61,10 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+        if (capturedPiece != null) {
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
 
@@ -84,6 +93,7 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
     private void initialSetup() {
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
